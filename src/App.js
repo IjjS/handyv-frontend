f565f00b2a971d835d0679a8be1c256f7d229a51
Jay <injaesong0@gmail.com>
2023-11-19 03:45:56 +0900

    feat: apply react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/common/header/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
