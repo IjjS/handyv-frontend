4d4646b883abb2bc06fc4fbc764f15dca0c7b9b7
Jay <injaesong0@gmail.com>
2023-11-18 21:50:44 +0900

    feat: add header

---
+++ src/components/common/header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import headerLogo from "../../../assets/png/handyV48.png";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-wrapper">
+        <div className="header-logo-wrapper">
+          <Link to="/">
+            <img src={headerLogo} alt="logo" />
+            <span>HandyV</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
